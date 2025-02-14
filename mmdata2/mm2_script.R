58c9fa6dcfbffc5807c3ca8e7135f4cdddbeeb99
khjloversclub <khjloversclub@gamil.com>
2025-02-14 19:46:40 +0900

    "Tutorial-2; MDS까지 완료. character로 인식된 데이터를 factor로 바꾸는 과정 필요하여 변경함."

--- mmdata2/mm2_script.R
+++ mmdata2/mm2_script.R
@@ -3,7 +3,7 @@
 # WD 설정
 getwd()
 setwd("/home/kimhyunjin/Rstd_khj/GitHub_khj/Rstd_bioinfo2")
-setwd("/home/kimhyunjin/Rstd_khj/GitHub_khj/Rstd_bioinfo2/mmdata2")
+list.files("/home/kimhyunjin/Rstd_khj/GitHub_khj/Rstd_bioinfo2/mmdata2")
 
 # data import
 library(edgeR)
@@ -42,7 +42,7 @@ View(countdata)
 # e.g. MCL1.DG_BC2CTUACXX_ACTTGA_L002_R1 -> MCL1.DG
 table(colnames(countdata)==sampleinfo$SampleName)
 
-# Filter some lowly expressed genes
+# Filter some lowly expressed genes(발현량 낮은 유전자 제거.)
 # Obtain CPMs (counts-per-million)
 myCPM <- cpm(countdata)
 # Have a look at the output
@@ -51,7 +51,9 @@ head(myCPM)
 col1sum <- sum(countdata[,1])/1000000
 countdata[1,1]/col1sum
 # Which values in myCPM are greater than 0.5?
+#참고 논문에서는 cpm값이 1 미만일 경우 미발현 유전자로 간주하여 거름.
 thresh <- myCPM > 0.5
+
 # This produces a logical matrix with TRUEs and FALSEs
 head(thresh)
 # Summary of how many TRUEs there are in each row
@@ -75,3 +77,98 @@ abline(v=0.5)
 # challenge 1
 plot(myCPM[,2],countdata[,2], xlab="CPM", ylab="Raw Count", main=colnames(myCPM)[2], ylim=c(0,50), xlim=c(0,3))
 abline(v=0.5, h=10, col="blue")
+
+
+
+
+## convert the counts to DGEList object(count data를 저장하는 기능)
+dgeObj <- DGEList(counts.keep) # 객체 만들기.
+# have a look at dgeObj
+dgeObj
+# See what slots are stored in dgeObj
+names(dgeObj)
+# Look at the structure of the list
+str(dgeObj)
+# Library size information is stored in the samples slot
+dgeObj$samples
+
+
+
+# QC
+## confirm; library sizes & distribution plots
+dgeObj$samples$lib.size
+# The names argument tells the barplot to use the sample names on the x-axis
+# The las argument rotates the axis names
+barplot(dgeObj$samples$lib.size, names=colnames(dgeObj), las=2)
+#### rep_len(width, NR)에서 다음과 같은 에러가 발생했습니다: NULL을 길이가 0인 객체에 복제할 수 없습니다
+########################################뭐가문제일까/ 0214.18:56 지금은 또 괜찮아졌네.
+
+#추가정보: 경고메시지(들):
+#  mean.default(width)에서:
+#  인자가 수치형 또는 논리형이 아니므로 NA를 반환합니다
+# Add a title to the plot
+title("Barplot of library sizes")
+
+# Get log2 counts per million; log2 scale에서 read count 분포 확인
+logcounts <- cpm(dgeObj,log=TRUE)
+# Check  density distributions of raw log-intensities of samples using boxplots
+boxplot(logcounts, xlab="", ylab="Log2 counts per million",las=2)
+# Let's add a blue horizontal line that corresponds to the median logCPM
+abline(h=median(logcounts),col="blue")
+title("Boxplots of logCPMs (unnormalised)")
+# 파란 수평선으로부터 멀리 떨어진 샘플은 특별히 더 조사할 필요 있음.
+## MCL1.LA, MCL1.LB, MCL1.LE, MCL1.LF 4가지가 가장 눈에 띔.
+
+
+
+
+
+# multi-dimensional scaling plot(MDSplot): 주성분 분석을 시각화하여 데이터에서 가장 큰 변인을 파악.
+plotMDS(dgeObj)
+# 그룹화 정보에 따라 샘플 분류(색상, 점, label 변경 등)
+# We specify the option to let us plot two plots side-by-side
+par(mfrow=c(1,2))
+# Let's set up colour schemes for CellType
+# How many cell types and in what order are they stored?
+levels(sampleinfo$CellType) ####### NULL #######
+
+######## troubleshooting with chatgpt #########
+head(sampleinfo)  # 데이터 미리보기
+str(sampleinfo)   # 데이터 구조 확인
+colnames(sampleinfo)  # 컬럼 이름 확인
+
+#CellType의 factor 여부; character or NULL 출력 > factor로 변환
+class(sampleinfo$CellType) # character 출력
+sampleinfo$CellType <- as.factor(sampleinfo$CellType)
+levels(sampleinfo$CellType)  # 다시 확인
+################# factor로 변환 성공 #################
+
+## Let's choose purple for basal and orange for luminal
+col.cell <- c("purple","orange")[sampleinfo$CellType]
+data.frame(sampleinfo$CellType,col.cell)
+col.cell
+sampleinfo$CellType
+
+# Redo the MDS with cell type colouring #################################
+plotMDS(dgeObj,col=col.cell)
+# Let's add a legend to the plot so we know which colours correspond to which cell type
+legend("topleft",fill=c("purple","orange"),legend=levels(sampleinfo$CellType))
+# Add a title
+title("Cell type")
+
+# Similarly for status ##트슛 타임##
+levels(sampleinfo$Status)
+col.status <- c("blue","red","dark green")[sampleinfo$Status]
+col.status
+plotMDS(dgeObj,col=col.status)
+legend("topleft",fill=c("blue","red","dark green"),legend=levels(sampleinfo$Status),cex=0.8)
+title("Status")
+######################범례가0으로나옴#######################
+#Status를 factor로 변경
+class(sampleinfo$Status) #character 출력
+sampleinfo$Status <- as.factor(sampleinfo$Status)
+levels(sampleinfo$Status)  # 다시 확인
+# 위의 코드 재실행/ 문제 해결 0214.19:42
+
+# Q:올바른 위치에 없는 것으로 보이는 두 샘플을 식별하세요.
+## 보이지 않는 두 샘플이 조사대상일 것.
